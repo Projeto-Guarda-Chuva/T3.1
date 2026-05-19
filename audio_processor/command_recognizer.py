549c4f8c4b48032a7c21a29107244e7a46df9c50
Giancarlo Teixeira <giancarlo.oliveira.teixeira@gmail.com>
2026-05-19 18:58:49 -0300

    feat: working audio processor based on vosk model - reading from an UDP socket and serving detected commands on a websocket

---
+++ audio_processor/command_recognizer.py
@@ -0,0 +1,46 @@
+import time
+
+import Levenshtein
+
+
+class CommandRecognizer:
+    def __init__(
+        self,
+        available_commands: list[str],
+        command_similarity_margin: float,
+        command_cooldown: int,
+    ):
+        self._command_similarity_margin = command_similarity_margin
+        self._available_commands = available_commands
+        self._command_cooldown = command_cooldown
+        self._command_last_detection = dict()
+
+    def recognize(self, word: str) -> str | None:
+        command = self._get_closest_command(word)
+
+        if command is None:
+            return None
+
+        now = time.time()
+
+        if command in self._command_last_detection:
+            last_detection = self._command_last_detection[command]
+            detection_interval = int((now - last_detection) * 1000)
+
+            if detection_interval < self._command_cooldown:
+                return None
+
+        return command
+
+    def _get_closest_command(self, word: str) -> str | None:
+        best_command = None
+        best_similarity = self._command_similarity_margin
+
+        for command in self._available_commands:
+            similarity = Levenshtein.ratio(command, word)
+
+            if similarity >= best_similarity:
+                best_command = command
+                best_similarity = similarity
+
+        return best_command
---
+++ audio_processor/config.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+
+from pydantic import BaseModel
+
+
+class AudioConfig(BaseModel):
+    sample_rate: int
+    sample_with: int
+    buffer_max_size_seconds: int
+    stream_host: str
+    stream_port: int
+
+
+class SpeechRecognitionConfig(BaseModel):
+    model_path: str
+    stability_window_ms: int
+    word_memory_size: int
+
+
+class CommandRecognitionConfig(BaseModel):
+    available_commands: list[str]
+    similarity_margin: float
+    command_cooldown_ms: int
+
+
+class GatewayConfig(BaseModel):
+    host: str
+    port: int
+
+
+class AppConfig(BaseModel):
+    audio: AudioConfig
+    speech_recognition: SpeechRecognitionConfig
+    command_recognition: CommandRecognitionConfig
+    gateway: GatewayConfig
+
+    @classmethod
+    def load_from_file(cls, filepath: str | Path) -> "AppConfig":
+        """Factory method to read the JSON file and parse it into this object."""
+        with open(filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return cls.model_validate(data)
