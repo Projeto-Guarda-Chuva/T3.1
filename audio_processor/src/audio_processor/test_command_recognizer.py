8b6e452bc09a2919246cf2eda84af476a314b5d6
Wallace Rosa <wallacerosa72@gmail.com>
2026-06-12 09:22:13 -0300

    removendo alguns comentários
    Ensure time module is imported at the top of the test file.

--- audio_processor/src/audio_processor/test_command_recognizer.py
+++ audio_processor/src/audio_processor/test_command_recognizer.py
@@ -1,6 +1,6 @@
 
 import pytest
-import time  # Certifique-se de importar o time no topo do arquivo de teste
+import time  
 from unittest.mock import patch
 from command_recognizer import CommandRecognizer
 
